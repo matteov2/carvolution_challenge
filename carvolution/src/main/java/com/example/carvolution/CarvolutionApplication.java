cca350fee7725cf490dbabc16d65aa7401f2db1a
Matteo Vögeli <mvo104224@iet-gibb.ch>
2022-09-16 11:40:33 +0200

    project initialisation

---
+++ carvolution/src/main/java/com/example/carvolution/CarvolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.carvolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarvolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarvolutionApplication.class, args);
+    }
+
+}
---
+++ carvolution/src/test/java/com/example/carvolution/CarvolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carvolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarvolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
